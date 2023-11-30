1934a0773cf91cec326774e829d637e9e832396f
BoiseITGuru <dev@boiseitguru.com>
2023-11-29 22:55:59 -0700

    depency fix

--- Package.swift
+++ Package.swift
@@ -15,8 +15,8 @@ let package = Package(
             targets: ["ecDAO"]),
     ],
     dependencies: [
-//        .package(url: "https://github.com/Forge4Flow/FlowComponents.git", from: "0.1.0"),
-        .package(name: "FlowComponents", path: "/Users/boiseitguru/Development/Forge4Flow/ecosystem_sdks/FlowComponents"),
+        .package(url: "https://github.com/Forge4Flow/FlowComponents.git", from: "0.2.0"),
+//        .package(name: "FlowComponents", path: "/Users/boiseitguru/Development/Forge4Flow/ecosystem_sdks/FlowComponents"),
     ],
     targets: [
         // Targets are the basic building blocks of a package, defining a module or a test suite.
